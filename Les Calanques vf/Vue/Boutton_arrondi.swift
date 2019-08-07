0d5b1d7951df6ca4dd1a4342ebfcae8c70f61165
TRAORE Lionel <traore.lionel@icloud.com>
2019-08-07 22:26:15 +0000

    Ajout d'un launch border au debut

---
+++ Les Calanques vf/Vue/Boutton_arrondi.swift
@@ -0,0 +1,26 @@
+//
+//  Boutton_arrondi.swift
+//  Les Calanques vf
+//
+//  Created by TRAORE Lionel on 8/7/19.
+//  Copyright © 2019 TRAORE Lionel. All rights reserved.
+//
+
+import UIKit
+
+class Boutton_arrondi: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup(){
+        layer.cornerRadius = 15
+    }
+}

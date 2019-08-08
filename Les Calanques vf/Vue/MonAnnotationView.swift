db3d345f9b24405b69774e736d30d2c010262687
TRAORE Lionel <traore.lionel@icloud.com>
2019-08-08 12:34:12 +0000

    Ajout des contraintes de façon programmatique

--- Les Calanques vf/Vue/MonAnnotationView.swift
+++ Les Calanques vf/Vue/MonAnnotationView.swift
@@ -33,18 +33,46 @@ class MonAnnotationView: MKAnnotationView {
     func setupLeft() -> UIButton {
         let button = UIButton(frame:  CGRect(x: 0, y: 0, width: 50, height: 50))
         button.setImage(UIImage(named: "distance"), for: .normal)
+        
+        //Ajout d'une action dans le button
+        button.addTarget(self, action: #selector(gps), for: .touchUpInside)
         return button
     }
     
     func setupRight() -> UIButton {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
         button.setImage(UIImage(named: "detail"), for: .normal)
+        
+        //Ajout d'une action dans le button
+        button.addTarget(self, action: #selector(detail), for: .touchUpInside)
         return button
     }
     
-    func setupCenter() -> UIView {
-        let view = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-        view.backgroundColor = .red
+    func setupCenter() -> UIView? {
+        //Vérification de l'exitence de l'image
+        guard let anno = annotation as? MonAnnotation else { return nil }
+        
+        //Définition de la taille de la frame qui servira à afficher les images
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 125, height: 125))
+        view.translatesAutoresizingMaskIntoConstraints = true
+        view.widthAnchor.constraint(equalToConstant: 125).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 125).isActive = true
+        
+        //Affiche de l'image au centre
+        let imageView = UIImageView(frame: view.bounds)
+        imageView.image = anno.calanque.image
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        view.addSubview(imageView)
+        
         return view
+    }
+    
+    @objc func detail() {
+        
+    }
+    
+    @objc func gps(){
+        
     }
 }
